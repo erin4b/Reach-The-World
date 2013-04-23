1e3a322d72ec5b82d2d9200ee547e915b026a704
Can Alptuna <orb@ob.noxio.us>
2013-04-23 11:00:10 -0400

    auto commit

---
+++ httpdocs/sites/default/files/js/js_ScL-BIS6bGoiYNx0OslhVf9lch2pCPQt1wuYxG5aD-w.js
@@ -0,0 +1,444 @@
+
+(function ($) {
+  Drupal.Panels = Drupal.Panels || {};
+
+  Drupal.Panels.autoAttach = function() {
+    if ($.browser.msie) {
+      // If IE, attach a hover event so we can see our admin links.
+      $("div.panel-pane").hover(
+        function() {
+          $('div.panel-hide', this).addClass("panel-hide-hover"); return true;
+        },
+        function() {
+          $('div.panel-hide', this).removeClass("panel-hide-hover"); return true;
+        }
+      );
+      $("div.admin-links").hover(
+        function() {
+          $(this).addClass("admin-links-hover"); return true;
+        },
+        function(){
+          $(this).removeClass("admin-links-hover"); return true;
+        }
+      );
+    }
+  };
+
+  $(Drupal.Panels.autoAttach);
+})(jQuery);
+;
+/**
+ * @file
+ * Adds some show/hide to the admin form to make the UXP easier.
+ */
+(function($){
+  Drupal.behaviors.video = {
+    attach: function (context, settings) {
+      //lets see if we have any jmedia movies
+      if($.fn.media) {
+        $('.jmedia').media();
+      }
+	
+      if(settings.video) {
+        $.fn.media.defaults.flvPlayer = settings.video.flvplayer;
+      }
+	
+      //lets setup our colorbox videos
+      $('.video-box').each(function() {
+        var url = $(this).attr('href');
+        var data = $(this).metadata();
+        var width = data.width;
+        var height= data.height;
+        var player = settings.video.player; //player can be either jwplayer or flowplayer.
+        $(this).colorbox({
+          html: '<a id="video-overlay" href="'+url+'" style="height:'+height+'; width:'+width+'; display: block;"></a>',
+          onComplete:function() {
+            if(player == 'flowplayer') {
+              flowplayer("video-overlay", settings.video.flvplayer, {
+                clip: {
+                  autoPlay: settings.video.autoplay,
+                  autoBuffering: settings.video.autobuffer
+                }
+              });
+            } else {
+              $('#video-overlay').media({
+                flashvars: {
+                  autostart: settings.video.autoplay
+                },
+                width:width,
+                height:height
+              });
+            }
+          }
+        });
+      });
+    }
+  };
+  
+  Drupal.behaviors.videoEdit = function(context){
+    // on change of the thumbnails when edit
+    $(".video-thumbnails input").each(function() {
+      var path = $(this).val();
+      if($(this).is(':checked')) {
+        var holder = $(this).attr('rel');
+        var id = $(this).attr('id');
+        var src = $('label[for="'+id+'"]').find('img').attr('src');
+        $('.'+holder+' img').attr('src', src);
+      }
+    });
+  }
+})(jQuery);
+;
+// $Id: extlink.js,v 1.8 2010/05/26 01:25:56 quicksketch Exp $
+(function ($) {
+
+function extlinkAttach(context) {
+  // Strip the host name down, removing ports, subdomains, or www.
+  var pattern = /^(([^\/:]+?\.)*)([^\.:]{4,})((\.[a-z]{1,4})*)(:[0-9]{1,5})?$/;
+  var host = window.location.host.replace(pattern, '$3$4');
+  var subdomain = window.location.host.replace(pattern, '$1');
+
+  // Determine what subdomains are considered internal.
+  if (Drupal.settings.extlink.extSubdomains) {
+    var subdomains = "([^/]*\\.)?";
+  }
+  else if (subdomain == 'www.' || subdomain == '') {
+    var subdomains = "(www\\.)?";
+  }
+  else {
+    var subdomains = subdomain.replace(".", "\\.");
+  }
+
+  // Build regular expressions that define an internal link.
+  var internal_link = new RegExp("^https?://" + subdomains + host, "i");
+
+  // Extra internal link matching.
+  var extInclude = false;
+  if (Drupal.settings.extlink.extInclude) {
+    extInclude = new RegExp(Drupal.settings.extlink.extInclude.replace(/\\/, '\\'));
+  }
+
+  // Extra external link matching.
+  var extExclude = false;
+  if (Drupal.settings.extlink.extExclude) {
+    extExclude = new RegExp(Drupal.settings.extlink.extExclude.replace(/\\/, '\\'));
+  }
+
+  // Find all links which are NOT internal and begin with http (as opposed
+  // to ftp://, javascript:, etc. other kinds of links.
+  // When operating on the 'this' variable, the host has been appended to
+  // all links by the browser, even local ones.
+  // In jQuery 1.1 and higher, we'd use a filter method here, but it is not
+  // available in jQuery 1.0 (Drupal 5 default).
+  var external_links = new Array();
+  var mailto_links = new Array();
+  $("a:not(." + Drupal.settings.extlink.extClass + ", ." + Drupal.settings.extlink.mailtoClass + ")", context).each(function(el) {
+    try {
+      var url = this.href.toLowerCase();
+      if (url.indexOf('http') == 0 && (!url.match(internal_link) || (extInclude && url.match(extInclude))) && !(extExclude && url.match(extExclude))) {
+        external_links.push(this);
+      }
+      else if (url.indexOf('mailto:') == 0) {
+        mailto_links.push(this);
+      }
+    }
+    // IE7 throws errors often when dealing with irregular links, such as:
+    // <a href="node/10"></a> Empty tags.
+    // <a href="http://user:pass@example.com">example</a> User:pass syntax.
+    catch(error) {
+      return false;
+    }
+  });
+
+  if (Drupal.settings.extlink.extClass) {
+    // Apply the "ext" class to all links not containing images.
+    if (parseFloat($().jquery) < 1.2) {
+      $(external_links).not('[img]').addClass(Drupal.settings.extlink.extClass).each(function() { if ($(this).css('display') == 'inline') $(this).after('<span class=' + Drupal.settings.extlink.extClass + '></span>'); });
+    }
+    else {
+      $(external_links).not($(external_links).find('img').parents('a')).addClass(Drupal.settings.extlink.extClass).each(function() { if ($(this).css('display') == 'inline') $(this).after('<span class=' + Drupal.settings.extlink.extClass + '></span>'); });
+    }
+  }
+
+  if (Drupal.settings.extlink.mailtoClass) {
+    // Apply the "mailto" class to all mailto links not containing images.
+    if (parseFloat($().jquery) < 1.2) {
+      $(mailto_links).not('[img]').addClass(Drupal.settings.extlink.mailtoClass).each(function() { if ($(this).css('display') == 'inline') $(this).after('<span class=' + Drupal.settings.extlink.mailtoClass + '></span>'); });
+    }
+    else {
+      $(mailto_links).not($(mailto_links).find('img').parents('a')).addClass(Drupal.settings.extlink.mailtoClass).each(function() { if ($(this).css('display') == 'inline') $(this).after('<span class=' + Drupal.settings.extlink.mailtoClass + '></span>'); });
+    }
+  }
+
+  if (Drupal.settings.extlink.extTarget) {
+    // Apply the target attribute to all links.
+    $(external_links).attr('target', Drupal.settings.extlink.extTarget);
+  }
+
+  if (Drupal.settings.extlink.extAlert) {
+    // Add pop-up click-through dialog.
+    $(external_links).click(function(e) {
+     return confirm(Drupal.settings.extlink.extAlertText);
+    });
+  }
+
+  // Work around for Internet Explorer box model problems.
+  if (($.support && !($.support.boxModel === undefined) && !$.support.boxModel) || ($.browser.msie && parseInt($.browser.version) <= 7)) {
+    $('span.ext, span.mailto').css('display', 'inline-block');
+  }
+}
+
+Drupal.behaviors.extlink = {
+  attach: function(context){
+    extlinkAttach(context);
+  }
+}
+
+})(jQuery);
+;
+(function ($) {
+
+Drupal.behaviors.textarea = {
+  attach: function (context, settings) {
+    $('.form-textarea-wrapper.resizable', context).once('textarea', function () {
+      var staticOffset = null;
+      var textarea = $(this).addClass('resizable-textarea').find('textarea');
+      var grippie = $('<div class="grippie"></div>').mousedown(startDrag);
+
+      grippie.insertAfter(textarea);
+
+      function startDrag(e) {
+        staticOffset = textarea.height() - e.pageY;
+        textarea.css('opacity', 0.25);
+        $(document).mousemove(performDrag).mouseup(endDrag);
+        return false;
+      }
+
+      function performDrag(e) {
+        textarea.height(Math.max(32, staticOffset + e.pageY) + 'px');
+        return false;
+      }
+
+      function endDrag(e) {
+        $(document).unbind('mousemove', performDrag).unbind('mouseup', endDrag);
+        textarea.css('opacity', 1);
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Automatically display the guidelines of the selected text format.
+ */
+Drupal.behaviors.filterGuidelines = {
+  attach: function (context) {
+    $('.filter-guidelines', context).once('filter-guidelines')
+      .find(':header').hide()
+      .closest('.filter-wrapper').find('select.filter-list')
+      .bind('change', function () {
+        $(this).closest('.filter-wrapper')
+          .find('.filter-guidelines-item').hide()
+          .siblings('.filter-guidelines-' + this.value).show();
+      })
+      .change();
+  }
+};
+
+})(jQuery);
+;
+
+(function ($) {
+
+Drupal.behaviors.tokenTree = {
+  attach: function (context, settings) {
+    $('table.token-tree', context).once('token-tree', function () {
+      $(this).treeTable();
+    });
+  }
+};
+
+Drupal.behaviors.tokenDialog = {
+  attach: function (context, settings) {
+    $('a.token-dialog', context).once('token-dialog').click(function() {
+      var url = $(this).attr('href');
+      var dialog = $('<div style="display: none" class="loading">' + Drupal.t('Loading token browser...') + '</div>').appendTo('body');
+
+      // Emulate the AJAX data sent normally so that we get the same theme.
+      var data = {};
+      data['ajax_page_state[theme]'] = Drupal.settings.ajaxPageState.theme;
+      data['ajax_page_state[theme_token]'] = Drupal.settings.ajaxPageState.theme_token;
+
+      dialog.dialog({
+        title: $(this).attr('title') || Drupal.t('Available tokens'),
+        width: 700,
+        close: function(event, ui) {
+          dialog.remove();
+        }
+      });
+      // Load the token tree using AJAX.
+      dialog.load(
+        url,
+        data,
+        function (responseText, textStatus, XMLHttpRequest) {
+          dialog.removeClass('loading');
+        }
+      );
+      // Prevent browser from following the link.
+      return false;
+    });
+  }
+}
+
+Drupal.behaviors.tokenInsert = {
+  attach: function (context, settings) {
+    // Keep track of which textfield was last selected/focused.
+    $('textarea, input[type="text"]', context).focus(function() {
+      Drupal.settings.tokenFocusedField = this;
+    });
+
+    $('.token-click-insert .token-key', context).once('token-click-insert', function() {
+      var newThis = $('<a href="javascript:void(0);" title="' + Drupal.t('Insert this token into your form') + '">' + $(this).html() + '</a>').click(function(){
+        if (typeof Drupal.settings.tokenFocusedField == 'undefined') {
+          alert(Drupal.t('First click a text field to insert your tokens into.'));
+        }
+        else {
+          var myField = Drupal.settings.tokenFocusedField;
+          var myValue = $(this).text();
+
+          //IE support
+          if (document.selection) {
+            myField.focus();
+            sel = document.selection.createRange();
+            sel.text = myValue;
+          }
+
+          //MOZILLA/NETSCAPE support
+          else if (myField.selectionStart || myField.selectionStart == '0') {
+            var startPos = myField.selectionStart;
+            var endPos = myField.selectionEnd;
+            myField.value = myField.value.substring(0, startPos)
+                          + myValue
+                          + myField.value.substring(endPos, myField.value.length);
+          } else {
+            myField.value += myValue;
+          }
+
+          $('html,body').animate({scrollTop: $(myField).offset().top}, 500);
+        }
+        return false;
+      });
+      $(this).html(newThis);
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Toggle the visibility of a fieldset using smooth animations.
+ */
+Drupal.toggleFieldset = function (fieldset) {
+  var $fieldset = $(fieldset);
+  if ($fieldset.is('.collapsed')) {
+    var $content = $('> .fieldset-wrapper', fieldset).hide();
+    $fieldset
+      .removeClass('collapsed')
+      .trigger({ type: 'collapsed', value: false })
+      .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Hide'));
+    $content.slideDown({
+      duration: 'fast',
+      easing: 'linear',
+      complete: function () {
+        Drupal.collapseScrollIntoView(fieldset);
+        fieldset.animating = false;
+      },
+      step: function () {
+        // Scroll the fieldset into view.
+        Drupal.collapseScrollIntoView(fieldset);
+      }
+    });
+  }
+  else {
+    $fieldset.trigger({ type: 'collapsed', value: true });
+    $('> .fieldset-wrapper', fieldset).slideUp('fast', function () {
+      $fieldset
+        .addClass('collapsed')
+        .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Show'));
+      fieldset.animating = false;
+    });
+  }
+};
+
+/**
+ * Scroll a given fieldset into view as much as possible.
+ */
+Drupal.collapseScrollIntoView = function (node) {
+  var h = document.documentElement.clientHeight || document.body.clientHeight || 0;
+  var offset = document.documentElement.scrollTop || document.body.scrollTop || 0;
+  var posY = $(node).offset().top;
+  var fudge = 55;
+  if (posY + node.offsetHeight + fudge > h + offset) {
+    if (node.offsetHeight > h) {
+      window.scrollTo(0, posY);
+    }
+    else {
+      window.scrollTo(0, posY + node.offsetHeight - h + fudge);
+    }
+  }
+};
+
+Drupal.behaviors.collapse = {
+  attach: function (context, settings) {
+    $('fieldset.collapsible', context).once('collapse', function () {
+      var $fieldset = $(this);
+      // Expand fieldset if there are errors inside, or if it contains an
+      // element that is targeted by the URI fragment identifier.
+      var anchor = location.hash && location.hash != '#' ? ', ' + location.hash : '';
+      if ($fieldset.find('.error' + anchor).length) {
+        $fieldset.removeClass('collapsed');
+      }
+
+      var summary = $('<span class="summary"></span>');
+      $fieldset.
+        bind('summaryUpdated', function () {
+          var text = $.trim($fieldset.drupalGetSummary());
+          summary.html(text ? ' (' + text + ')' : '');
+        })
+        .trigger('summaryUpdated');
+
+      // Turn the legend into a clickable link, but retain span.fieldset-legend
+      // for CSS positioning.
+      var $legend = $('> legend .fieldset-legend', this);
+
+      $('<span class="fieldset-legend-prefix element-invisible"></span>')
+        .append($fieldset.hasClass('collapsed') ? Drupal.t('Show') : Drupal.t('Hide'))
+        .prependTo($legend)
+        .after(' ');
+
+      // .wrapInner() does not retain bound events.
+      var $link = $('<a class="fieldset-title" href="#"></a>')
+        .prepend($legend.contents())
+        .appendTo($legend)
+        .click(function () {
+          var fieldset = $fieldset.get(0);
+          // Don't animate multiple times.
+          if (!fieldset.animating) {
+            fieldset.animating = true;
+            Drupal.toggleFieldset(fieldset);
+          }
+          return false;
+        });
+
+      $legend.append(summary);
+    });
+  }
+};
+
+})(jQuery);
+;
